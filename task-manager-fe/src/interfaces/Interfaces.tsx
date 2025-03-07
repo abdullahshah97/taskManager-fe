aa69b7df23c86211aa2a3f01b46d05bbcd5ea4e8
abdullahshah97 <abdullah-shah97@hotmail.com>
2025-03-07 10:19:25 +0000

    Interfaces file to store all interfaces

---
+++ task-manager-fe/src/interfaces/Interfaces.tsx
@@ -0,0 +1,17 @@
+export interface I_ResponseData {
+    success: boolean;
+    message?: string;
+    token ?: string;
+}
+
+export interface I_Task {
+    id: number;
+    name: string;
+    description: string;
+    tags: string[];
+}
+
+export interface I_UserAuth {
+    userId: string;
+    token: string;
+}
