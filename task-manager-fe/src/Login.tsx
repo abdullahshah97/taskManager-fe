13bfad666050c0060f43c4f48a27ebd2784b7852
abdullahshah97 <abdullah-shah97@hotmail.com>
2025-03-08 22:13:45 +0000

    Pass user token to context provider after making the mock call

--- task-manager-fe/src/Login.tsx
+++ task-manager-fe/src/Login.tsx
@@ -1,17 +1,20 @@
 import {useEffect, useState} from "react";
 import {mockLoginRequest} from "./mockServices/MockLoginRequest";
 import {I_ResponseData} from "./interfaces/Interfaces";
+import {useAuth} from "./contextProvider/AuthContext";
 
 const Login = () => {
     const [submitted, setSubmitted] = useState(false);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [response, setResponse] = useState<I_ResponseData | null>(null);
+    const {setToken} = useAuth();
 
     useEffect(() => {
         if (submitted) {
             mockLoginRequest(email, password).then((res) => {
                 setResponse(res);
+                setToken(res.token);
                 setSubmitted(false);
             });
         }
