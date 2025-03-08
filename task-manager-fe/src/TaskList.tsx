b3d07cf8cda7f70d027008ba32093ed429e227b0
abdullahshah97 <abdullah-shah97@hotmail.com>
2025-03-08 22:12:42 +0000

    beta: display list view task mock data response,

--- task-manager-fe/src/TaskList.tsx
+++ task-manager-fe/src/TaskList.tsx
@@ -1,25 +1,52 @@
 import {useEffect, useState} from "react";
 import {mockTaskListRequest} from "./mockServices/MockTaskListRequest";
-import {I_Task} from "./interfaces/Interfaces";
+import {I_Task, I_UserAuth} from "./interfaces/Interfaces";
+import {useAuth} from "./contextProvider/AuthContext";
 
 
 const TaskList = () => {
+    const authToken = sessionStorage.getItem("token");
     const [taskList, setTaskList] = useState<I_Task[]>([]);
-    const [user, setUser] = useState("");
+    // @ts-ignore
+    const [user, setUser] = useState<I_UserAuth>({userId: "abs@hotmail.com", token: authToken});
 
     useEffect(() => {
         // fetch all the lists according to user
         mockTaskListRequest(user).then((res) => {
-
+            setTaskList(res);
+            console.log(user);
         })
 
-    }, [taskList]);
+    }, []);
 
     const updateTable = () => {
 
     }
     return(
-        <></>
+        <>
+        <table>
+            <thead>
+                <tr>
+                    {}
+                </tr>
+            </thead>
+            <tbody>
+            {taskList.length === 0 ? (
+                <tr>
+                    <td>No tasks available</td>
+                </tr>
+            ) : (
+                taskList.map(task => (
+                    <tr key={task.id}>
+                        <td>{task.name}</td>
+                        <td>{task.description}</td>
+                        <td>{task.tags}</td>
+                    </tr>
+                ))
+            )}
+            </tbody>
+        </table>
+        </>
     )
 }
 
