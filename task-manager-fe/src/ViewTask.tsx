34187ed0a8cbffa56b85f6d9f64c547df029158a
abdullahshah97 <abdullah-shah97@hotmail.com>
2025-03-10 22:34:59 +0000

    handle closing the dialogue box, with placeholders for edit delete and done

--- task-manager-fe/src/ViewTask.tsx
+++ task-manager-fe/src/ViewTask.tsx
@@ -1,12 +1,19 @@
 import {I_Task} from "./interfaces/Interfaces";
 import {useState} from "react";
 
-const ViewTask = (passedTask: I_Task) => {
+const ViewTask = ({ passedTask, onClose} : {passedTask:I_Task, onClose: () => void}) => {
     const [task, setTask] = useState<I_Task>(passedTask);
 
-    //pop up box with headers and individual task
-    //option to delete or update
-
+    return(
+        <dialog open>
+            <p>{passedTask.name}</p>
+            <form method="dialog">
+                <button onClick = {onClose}>Edit Task</button>
+                <button onClick = {onClose}>Delete Task</button>
+                <button onClick = {onClose}>Done</button>
+            </form>
+        </dialog>
+    )
 }
 
 export default ViewTask;
