ce49fe3ebd8b5e032e5385489d400263dcf2173b
abdullahshah97 <abdullah-shah97@hotmail.com>
2025-03-10 22:14:06 +0000

    fix: clean up some imports

--- task-manager-fe/src/App.tsx
+++ task-manager-fe/src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Signup from "./Signup";
-import { AuthProvider, useAuth } from "./contextProvider/AuthContext";
+import { useAuth } from "./contextProvider/AuthContext";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import Logout from "./Logout";
 import Login from "./Login";
 import TaskList from "./TaskList";
-import ReactDOM from "react-dom";
 
 const App = () => {
     const { token } = useAuth();
